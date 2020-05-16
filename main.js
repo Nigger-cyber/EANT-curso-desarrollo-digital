c0cbaec7d92a081f921152be9147b08ce20ec29f
Nigger-cyber <lucasjuarezsibello@gmail.com>
2020-05-16 13:37:54 -0300

    Creando server local para JSON local

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const server = express()
+
+const docs = express.static("docs")
+
+server.use(docs)
+
+server.get("/pepito.html", function(request, response){ // Con ctrl + c apago la consola
+    response.end("Acá van a mostrarse las ofertas MercadoTECH")
+})
+
+//server.post()
+
+server.listen(3000)
